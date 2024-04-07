1efa85430b99cb40a2a66ac191547994d5916787
rupokghosh <rupokghosh291@gmail.com>
2024-04-07 12:01:13 -0700

    add components css

--- clientside/src/components/Chat.jsx
+++ clientside/src/components/Chat.jsx
@@ -4,9 +4,9 @@ const Chat = ({ messages }) => {
   return (
     <div>
       {messages.map((msg, index) => (
-        <div key={index} className="message">
-          <p>{msg.sender}</p>
-          <p>{msg.message}</p>
+        <div key={index} className="message bg-blue-100 rounded-lg p-2 mb-2">
+          <div className="text-xs font-bold">{msg.sender}</div>
+          <div className="bg-white rounded-lg p-2">{msg.message}</div>
         </div>
       ))}
     </div>
--- clientside/src/components/Header.jsx
+++ clientside/src/components/Header.jsx
@@ -2,7 +2,7 @@ import "../index.css";
 const Header = () => {
   return (
     <div className="font-bold mb-4 p-4 bg-blue-400 text-center text-2xl">
-      <h1>mini messsage board</h1>
+      <h1>mini message board</h1>
     </div>
   );
 };
