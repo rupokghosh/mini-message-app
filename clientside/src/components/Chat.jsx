c3b1a51a5caadd5ca06c20409ab0e05bd044b159
rupokghosh <rupokghosh291@gmail.com>
2024-04-09 12:42:03 -0700

    change text-box color;

--- clientside/src/components/Chat.jsx
+++ clientside/src/components/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = ({ messages }) => {
       {messages.map((msg, index) => (
         <div key={index} className="message ml-4 rounded-lg p-2 mb-2">
           <div className="text-xs font-bold">{msg.sender}</div>
-          <div className="bg-blue-400 rounded-lg p-2">{msg.message}</div>
+          <div className="bg-pink-400 rounded-lg p-2">{msg.message}</div>
         </div>
       ))}
     </div>
